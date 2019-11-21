8dad6ec39bb1ed9c8cfb046b5277fc9d4abd987e
matheusps <cout.matheusps@gmail.com>
2019-11-20 21:04:38 -0300

    starting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react'
+import { importMDX } from 'mdx.macro'
+import { ExperimentalSpinner as Spinner } from 'adapt-ui'
+
+const Content = lazy(() => importMDX('./Content.mdx'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Suspense fallback={<Spinner size="xl" />}>
+        <Content />
+      </Suspense>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
